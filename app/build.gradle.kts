1e844e7e1622e51340e42afc842cc0daaa3f7f58
Khushi-Gangani <gang0025@algonquinlive.com>
2024-03-27 17:24:36 -0400

    finish lab6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.accessors.AccessorFormats.default
+
 plugins {
     id("com.android.application")
 }
@@ -6,9 +8,14 @@ android {
     namespace = "algonquin.CST2335.pate1214"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+        aidl=true
+    }
+
     defaultConfig {
         applicationId = "algonquin.CST2335.pate1214"
-        minSdk = 27
+        minSdk = 22
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -29,11 +36,14 @@ android {
 }
 
 dependencies {
-
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
